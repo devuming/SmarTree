c58906eaf5cc817528fc8445feaea34ec67ce650
김유민 <doumeano0o@gmail.com>
2022-12-19 04:44:24 +0900

    led, message good

--- SmarTree.ino
+++ SmarTree.ino
@@ -1,18 +1,20 @@
 #include <Adafruit_NeoPixel.h>
-#include<LiquidCrystal_I2C.h>
-#include<Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 //---- Sensor ----
-
+#define LDR 3       // 조도센서
+int old_light = 0;  // 이전 밝기
 
 //---- Actuator ----
 #define NEO_PIN 11
-#define NUMPIXELS 50 // Popular NeoPixel ring size
+#define NUMPIXELS 50  // 사용할 NeoPixel 수
+#define BRIGHTNESS 125 
 
 Adafruit_NeoPixel pixels(NUMPIXELS, NEO_PIN, NEO_GRB + NEO_KHZ800);
 int start_pixel = 0;
 
-#define BUZZER 8
-LiquidCrystal_I2C lcd(0x27, 16, 2);   // LCD I2C 연결 (2행 16열)
+#define BUZZER 8                     // 수동부저
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // LCD I2C 연결 (2행 16열)
 
 //---- Global ----
 String user_id = "1";
@@ -22,15 +24,18 @@ String old_data = "";
 
 //---- Slave ----
 void setup() {
-  Wire.begin(1);   
+  Wire.begin(1);
   Wire.onReceive(receiveEvent);
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  Wire.onRequest(requestEvent);
+  pixels.begin();  // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.clear();
+  pixels.show();
   lcd.begin();
   lcd.backlight();
   // lcd.print("Hello");
-  
+
   Serial.begin(115200);
-  
+
   user_id = "1";
   message = "hello";
   data = "";
@@ -38,129 +43,163 @@ void setup() {
 }
 void loop() {
   // onNeoPixels_rainbow();
-  if(data != "" && !data.equals(old_data)){
+
+  if (data != "" && !data.equals(old_data)) {
     parsingDone(data);
     Serial.print("msg : ");
     Serial.println(message);
     lcd.clear();
-    lcd.setCursor(0,0);
-    if(message.length()>16){
+    lcd.setCursor(0, 0);
+    if (message.length() > 16) {
       lcd.print(message.substring(0, 15));
-      lcd.setCursor(0,1);
+      lcd.setCursor(0, 1);
       lcd.print(message.substring(15, 30));
-    }else{
+    } else if (message.length() > 0 && message.length() < 16) {
       lcd.print(message);
     }
+    else if (message.length() == 0) {
+      lcd.print("No MESSAGE:)");
+    }
+
+    Serial.println("-----------");
+    Serial.println(old_data);
+    Serial.println(data);
+    Serial.println("-----------");
     old_data = data;
   }
-  
+
   delay(100);
+}
+
+// Master 요청 받은 경우 빛 감지
+void requestEvent() {
+  // 빛 감지
+  if (old_light == 0) {
+    old_light = analogRead(LDR);
+  }
+  int val = analogRead(LDR);
+  Serial.print("LDR:");
+  Serial.print(old_light);
+  Serial.print(",");
+  Serial.print(val);
+  if (old_light - val > 300) {
+    Wire.write('1');
+  } else {
+    Wire.write('0');
+  }
+
+  old_light = val;
+  delay(1000);
 }
 
 String temp = "";
 int count_sharp = 0;  // start-end flag
-void receiveEvent(int howMany){
+
+void receiveEvent(int howMany) {
+  Serial.println(howMany);
   String sMessage = "";
 
-  while (Wire.available()) { 
+  while (Wire.available()) {
     char c = Wire.read();
     Serial.print(c);
-    
-    
-    // sMessage = Wire.readStringUntil('\n');  
-    // Serial.println(sMessage); 
-    if (c == '#')
-      count_sharp += 1;
-    else
-      sMessage += c;
 
-    if (count_sharp == 2)
-      break;
+    if (count_sharp < 2) {
+      if (c == '#')
+        count_sharp += 1;
+      else
+        sMessage += c;
+    } else
+      Wire.read();
   }
   Serial.println();
   temp += sMessage;
-  
-  Serial.println(sMessage);
-  Serial.println(temp);
-    // Serial.println(); 
-  // if(sMessage.indexOf('#') != -1)
-  // {
-  //   // Serial.println("###"); 
-  //   // temp += sMessage;
-  //   // Serial.println(temp);
-  //   if (count_sharp == 2)
-  //   {
-  //     data = temp;
-  //     Serial.print('data : ');
-  //     Serial.println(data);
-  //     count_sharp = 0;
-  //   }
-  // }
 
-  if (count_sharp == 2)
-  {
+  if (count_sharp == 2) {
     data = temp;
-    Serial.print('data : ');
+    Serial.print("data");
     Serial.println(data);
     count_sharp = 0;
     temp = "";
   }
-  delay(1000);
+  delay(100);
 }
 
 // 할일목록 조회 내용 파싱
-void parsingDone(String res){
+void parsingDone(String res) {
   String color = "";
   int total_cnt = 0;
   int done_cnt = 0;
+  int msg_positive = 0;
+  Serial.print("hey:");
+  Serial.println(res);
 
-   if (res.indexOf('|') != -1){
-     int idx = res.indexOf('|');
-     color = res.substring(0, idx);
+  if (res.indexOf('|') != -1) {
+    // 1. 색상
+    int idx = res.indexOf('|');
+    if (idx > 0) {
+      color = res.substring(0, idx);
+    } else {
+      color = "";
+      offNeoPixels();
+    }
+    res = res.substring(idx + 1);
 
-     // 갯수
-     res = res.substring(idx + 1);
-     if(res.indexOf('|') != -1)
-     {
+    // 2. todo 갯수
+    if (res.indexOf('|') != -1 && res.indexOf(',') != -1) {
       idx = res.indexOf('|');            // 두번째 | Split
-      if (res.indexOf(',') != -1)
-      {
-        int cnt_idx = res.indexOf(',');    // , Split
-        total_cnt = res.substring(0, cnt_idx).toInt();
-        done_cnt = res.substring(cnt_idx+1, idx).toInt();
-        if(idx + 1 < res.length())
-          message = res.substring(idx + 1);
-        else
+      int comma_idx = res.indexOf(',');  // , Split
+      total_cnt = res.substring(0, comma_idx).toInt();
+      done_cnt = res.substring(comma_idx + 1, idx).toInt();
+
+      // if (idx + 1 > res.length())
+      //   message = res.substring(idx + 1);
+      // else
+      //   message = "";
+
+      // 3. message
+      res = res.substring(idx + 1);
+      if (res.indexOf(',') != -1) {
+        comma_idx = res.indexOf(',');
+        if (comma_idx == 0)
           message = "";
-          
-        Serial.print(total_cnt);
-        Serial.print("/");
-        Serial.print(done_cnt);
-        Serial.print("/");
-        Serial.println(message);
-        if(total_cnt == done_cnt)
-        {
-          onNeoPixels_rainbow();
-        }else{
-          coloring(color, total_cnt, done_cnt);
-        }
+        else
+          message = res.substring(0, comma_idx);
+        msg_positive = res.substring(comma_idx + 1).toInt();
       }
-     }
-   
-    delay(100);
-   }
+
+      Serial.println("parsing done");
+      if ((total_cnt > 0 && total_cnt == done_cnt) || msg_positive == 10) {
+        // 모두완료
+        onNeoPixels_rainbow();
+        rudolph();
+      } else {
+        coloring(color, total_cnt, done_cnt);
+      }
+    }
+  }
+
+  delay(100);
 }
 
+
 // 할일 완료 여부에 따른 LED ON 처리
-void coloring(String color, int total, int done){
+void coloring(String color, int total, int done) {
+  if (color.length() == 0) {
+    offNeoPixels();
+    return;
+  }
   // 시작 픽셀 초기화
   start_pixel = 0;
   int color_cnt = color.length() / 9;
   pixels.clear();
 
-  int pixel_count = NUMPIXELS * (float(done)/float(total)) / done;
-  for(int i = 0; i < color_cnt; i++)
-  {
+  int pixel_count = 0;
+  if (done > 0)
+    pixel_count = NUMPIXELS * (float(done) / float(total)) / done;
+  else
+    pixel_count = NUMPIXELS * (float(done) / float(total));
+
+  for (int i = 0; i < color_cnt; i++) {
     int r = color.substring((i * 9), (i * 9) + 3).toInt();
     int g = color.substring((i * 9) + 3, (i * 9) + 6).toInt();
     int b = color.substring((i * 9) + 6, (i * 9) + 9).toInt();
@@ -176,5 +215,5 @@ void coloring(String color, int total, int done){
     onNeoPixels(r, g, b, pixel_count);
   }
 
-  delay(10000);
+  delay(1000);
 }
--- controlNeopixels.ino
+++ controlNeopixels.ino
@@ -8,6 +8,7 @@ int color[7][3] = {{255, 0, 0},   // 빨
                   };
 void onNeoPixels(int r, int g, int b, int count){
   // pixels.clear(); // Set all pixel colors to 'off'
+  pixels.setBrightness(BRIGHTNESS);
   
   for(int i=0; i<count; i++) { // For each pixel...
     pixels.setPixelColor(start_pixel + i, pixels.Color(r, g, b));
@@ -21,6 +22,7 @@ void onNeoPixels(int r, int g, int b, int count){
 void onNeoPixels_rainbow(){
   pixels.clear(); // Set all pixel colors to 'off'
   int c = 0;
+  pixels.setBrightness(BRIGHTNESS);
 
   for(int i=0; i<NUMPIXELS; i++) {
     pixels.setPixelColor(i, pixels.Color(color[c][0], color[c][1], color[c][2]));
@@ -29,7 +31,19 @@ void onNeoPixels_rainbow(){
       c = 0;
     else
       c += 1;
-    delay(100); // Pause before next pass through loop
+    delay(10); // Pause before next pass through loop
   }
   pixels.rainbow(0);
+}
+void offNeoPixels(){
+  // pixels.clear(); // Set all pixel colors to 'off'
+  
+  for(int i = BRIGHTNESS; i >= 0; i--)
+  {
+    pixels.setBrightness(i);
+    pixels.show(); 
+  }
+  pixels.clear(); 
+  delay(100);
+  
 }
